4576ecfdc496e5eab8eb01ee80e47ec572c74546
Nathaniel Burke <nathanielrburke3@gmail.com>
2026-01-17 13:31:30 +0100

    Check @rew constraints in applications

--- src/full/Agda/Interaction/Options/Warnings.hs
+++ src/full/Agda/Interaction/Options/Warnings.hs
@@ -369,6 +369,7 @@ data WarningName
   | RewriteAmbiguousRules_
   | RewriteMissingRule_
   | DuplicateRewriteRule_
+  | LetBoundLocalRewrite_
   | SafeFlagEta_
   | SafeFlagInjective_
   | SafeFlagNoCoverageCheck_
@@ -620,6 +621,7 @@ warningNameDescription = \case
   RewriteAmbiguousRules_           -> "Failed global confluence checks because of overlapping rules."
   RewriteMissingRule_              -> "Failed global confluence checks because of missing rule."
   DuplicateRewriteRule_            -> "Duplicate rewrite rules."
+  LetBoundLocalRewrite_            -> "Let-bound variable annotated with '@rew'."
   SafeFlagEta_                     -> "`ETA' pragmas with the safe flag."
   SafeFlagInjective_               -> "`INJECTIVE' pragmas with the safe flag."
   SafeFlagNoCoverageCheck_         -> "`NON_COVERING` pragmas with the safe flag."
--- src/full/Agda/TypeChecking/Substitute.hs
+++ src/full/Agda/TypeChecking/Substitute.hs
@@ -997,6 +997,15 @@ instance Subst RewriteRule where
                   c top
     where n = size gamma
 
+instance Subst LocalEquation where
+  type SubstArg LocalEquation = Term
+  applySubst rho (LocalEquation gamma lhs rhs t) =
+    LocalEquation (applySubst rho gamma)
+                  (applySubst rho' lhs)
+                  (applySubst rho' rhs)
+                  (applySubst rho' t)
+    where rho' = liftS (size gamma) rho
+
 instance Subst a => Subst (Blocked a) where
   type SubstArg (Blocked a) = SubstArg a
   applySubst rho b = fmap (applySubst rho) b
@@ -1561,6 +1570,7 @@ deriving instance Eq DefSing
 deriving instance Eq NLPat
 deriving instance Eq NLPType
 deriving instance Eq NLPSort
+deriving instance Eq LocalEquation
 deriving instance Eq LocalRewriteHead
 deriving instance Eq LocalRewriteRule
 
@@ -1568,6 +1578,7 @@ deriving instance Ord DefSing
 deriving instance Ord NLPSort
 deriving instance Ord NLPType
 deriving instance Ord NLPat
+deriving instance Ord LocalEquation
 deriving instance Ord LocalRewriteHead
 deriving instance Ord LocalRewriteRule
 
